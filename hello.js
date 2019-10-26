f08d9cca407f7c042960db7ab19a8093bcd174b0
Hein <hein@journeyapps.com>
2019-10-26 08:11:56 +0200

    first commit made

--- hello.js
+++ hello.js
@@ -9,3 +9,4 @@ console.log(`okay... Bye!!`);
 
 
 console.log(`Here is some other stuff that was written in the testing branch and should always be present here from now on...`);
+console.log('[testing] first commit made');
