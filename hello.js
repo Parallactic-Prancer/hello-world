45041232aeddb72b2e4d05cc9eb81c940749a317
Hein <hein@journeyapps.com>
2019-10-26 08:18:55 +0200

    2nd commit, first one from master after a commit took place in testing

--- hello.js
+++ hello.js
@@ -9,3 +9,12 @@ console.log(`okay... Bye!!`);
 
 
 console.log(`Here is some other stuff that was written in the testing branch and should always be present here from now on...`);
+
+
+
+
+
+
+
+
+console.log(`2. [master] Hope this message finds you well in the under world!`);
