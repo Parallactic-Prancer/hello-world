17612bacc79b2e14a42c3e8101c5605575eb098c
Hein <hein@journeyapps.com>
2019-10-26 07:57:41 +0200

    added a file and mc in line 5

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 let name = process.argv[2];
-if(!name) {
+if (!name) {
     name = 'Stranger';
 }
-console.log(`Hello ${name}.`);
+console.log(`Hello ${name}. This is the MASTER BRANCH`);
 console.log(`See you later, ${name}.`);
 console.log(`okay... Bye!!`);
