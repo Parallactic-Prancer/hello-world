14606f0c5181179514d46e28b1807407abf4a60d
Hein <hein@journeyapps.com>
2019-10-26 07:54:25 +0200

    one file added and one change on line 5 that should create a merge conflict

--- hello.js
+++ hello.js
@@ -1,7 +1,11 @@
 let name = process.argv[2];
-if(!name) {
+if (!name) {
     name = 'Stranger';
 }
-console.log(`Hello ${name}.`);
+console.log(`Hello ${name}. This is the Testing Branch!!`);
 console.log(`See you later, ${name}.`);
 console.log(`okay... Bye!!`);
+
+
+
+console.log(`Here is some other stuff that was written in the testing branch and should always be present here from now on...`);
