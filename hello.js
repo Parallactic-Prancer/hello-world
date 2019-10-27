8fba676585faaea5a0de0fc71cf127fc5af78468
Hein <hein@journeyapps.com>
2019-10-27 18:11:39 +0200

    Added something new in master

--- hello.js
+++ hello.js
@@ -6,3 +6,8 @@ console.log(`1.) [testing] 1st commit change`);
 console.log(`2 [master] 1st commit`);
 
 
+
+console.log(`adding something new in master branch`);
+console.log(`3 [master] 2nd commuit`);
+
+
